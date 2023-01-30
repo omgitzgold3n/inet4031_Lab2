d5dc8976dc65b0768225db17897db771f11ef5b4
Christopher Golden <101673121+omgitzgold3n@users.noreply.github.com>
2023-01-29 19:32:15 -0600

    Update fileprocessor.py

--- fileprocessor.py
+++ fileprocessor.py
@@ -4,7 +4,7 @@ list = []
 print("Printing out User Data:\n")
 for line in infile:
 	if(line[0] == '#'):
-		print("The user "+line[1:6]+" is skipped becuase it starts with a hashtag (is commented out)")
+		print("The user "+line[1:6]+" is skipped because it starts with a hashtag (is commented out)")
 	else:
 		var = line.split(':')
 		print("The user " + var[0],"has a password of "+var[1]+" and has a userid "+var[2],"and groupid " + var[3])
